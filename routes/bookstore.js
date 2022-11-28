12dcb40ff217fec7eeeeb43ec1e78fa51eda4cad
anna_joachimska <ajoachimska@bee2code.com>
2022-11-28 15:00:37 +0100

    removed unnecessary variable

--- routes/bookstore.js
+++ routes/bookstore.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const router = express.Router();
 const bookstoreController = require('../controllers/bookstoreController')
-const publishingHousesController = require("../controllers/publisherHousesController");
 
 router.get('/', bookstoreController.getAllBookstores);
 
