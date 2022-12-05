66b55e332fab26ae327874a43a30bbe01919d9f6
anna_joachimska <ajoachimska@bee2code.com>
2022-12-05 14:54:09 +0100

    added schema validation

--- models/book.js
+++ models/book.js
@@ -1,11 +1,16 @@
 const mongoose = require('mongoose');
 
 const bookSchema = mongoose.Schema({
-    name: {type: String, required: true, unique: true, minLength: 3, maxLength: 50},
-    type: {type: String, required: true,
-        enum: ['Kryminał', "Dramat", "Pamiętnik", "Romans",
-            "Dla dzieci", "Fantasy", "Horror", "Sci-Fi", "Powieść historyczna",
-            "Bibliografia", "Reportaż", "Powieść młodzieżowa", "Poradnik", "Kucharska"]},
+    name: {type: String, required: true, unique: true, minLength: 3, maxLength: 50,},
+    type: {
+        type: String, required: true,
+        enum: {
+            values: ['Kryminał', "Dramat", "Pamiętnik", "Romans",
+                "Dla dzieci", "Fantasy", "Horror", "Sci-Fi", "Powieść historyczna",
+                "Bibliografia", "Reportaż", "Powieść młodzieżowa", "Poradnik", "Kucharska"],
+            message: '{VALUE} is not supported'
+        },
+    },
     publishingHouse: {type: mongoose.Types.ObjectId, ref: 'PublishingHouse'},
     bookstores: [{type: mongoose.Types.ObjectId, ref: 'Bookstore'}]
     });
