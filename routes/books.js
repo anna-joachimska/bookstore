c7fa141fdca1136fc5fc06c79b690944b5610f94
anna_joachimska <ajoachimska@bee2code.com>
2022-12-14 08:59:24 +0100

    added new get endpoints

--- routes/books.js
+++ routes/books.js
@@ -4,6 +4,10 @@ const booksController = require('../controllers/booksController')
 
 router.get('/', booksController.getAllBooks);
 
+router.get('/WithPublishingHouses', booksController.getAllBooksWithPublishingHouses);
+
+router.get('/WithBookstores', booksController.getAllBooksWithBookstores);
+
 router.post('/', booksController.createNewBook);
 
 router.get('/:bookId', booksController.getBook);
@@ -11,5 +15,13 @@ router.get('/:bookId', booksController.getBook);
 router.patch('/:bookId', booksController.updateBook);
 
 router.delete('/:bookId', booksController.deleteBook);
+
+router.patch('/deleteBookstoreFromBook/:bookId', booksController.deleteBookstoreFromBook);
+
+router.patch('/addBookstoreToBook/:bookId', booksController.addBookstoreToBook);
+
+router.patch('/deletePublishingHouseFromBook/:bookId', booksController.deletePublishingHouseFromBook);
+
+router.patch('/addPublishingHouseToBook/:bookId', booksController.addPublishingHouseToBook);
 
 module.exports = router;
--- routes/bookstore.js
+++ routes/bookstore.js
@@ -4,6 +4,10 @@ const bookstoreController = require('../controllers/bookstoreController')
 
 router.get('/', bookstoreController.getAllBookstores);
 
+router.get('/WithBooks', bookstoreController.getAllBookstoresWithBooks);
+
+router.get('/WithPublishingHouses', bookstoreController.getAllBookstoresWithPublishingHouses);
+
 router.post('/', bookstoreController.createNewBookstore);
 
 router.get('/:bookstoreId', bookstoreController.getBookstore);
@@ -11,5 +15,13 @@ router.get('/:bookstoreId', bookstoreController.getBookstore);
 router.patch('/:bookstoreId', bookstoreController.updateBookstore);
 
 router.delete('/:bookstoreId', bookstoreController.deleteBookstore);
+
+router.patch('/deleteBookFromBookstore/:bookstoreId', bookstoreController.deleteBookFromBookstore);
+
+router.patch('/addBookToBookstore/:bookstoreId', bookstoreController.addBookToBookstore);
+
+router.patch('/deletePublishingHouseFromBookstore/:bookstoreId', bookstoreController.deletePublishingHouseFromBookstore);
+
+router.patch('/addPublishingHouseToBookstore/:bookstoreId', bookstoreController.addPublishingHouseToBookstore);
 
 module.exports = router;
